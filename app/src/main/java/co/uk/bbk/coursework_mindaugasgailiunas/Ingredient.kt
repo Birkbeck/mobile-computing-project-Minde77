c8ce3162f2518ee2173406eeb338c80f567fbc32
Mindaugas Gailiunas <mgaili01@student.bbk.ac.uk>
2025-05-28 19:15:19 +0100

    Add class comment

--- app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/Ingredient.kt
+++ app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/Ingredient.kt
@@ -1,3 +1,9 @@
 package co.uk.bbk.coursework_mindaugasgailiunas
 
+/**
+ * An Ingredient data class contains ingredient text.
+ *
+ * @author Mindaugas Gailiunas
+ * @property text an Ingredient description
+ */
 data class Ingredient()
