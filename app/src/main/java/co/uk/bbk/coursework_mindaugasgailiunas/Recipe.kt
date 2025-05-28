8a789cf0b8a119f8ef0f06379c16212b1e865e2d
Mindaugas Gailiunas <mgaili01@student.bbk.ac.uk>
2025-05-28 19:09:57 +0100

    Add class comment

--- app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/Recipe.kt
+++ app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/Recipe.kt
@@ -1,3 +1,12 @@
 package co.uk.bbk.coursework_mindaugasgailiunas
 
+/**
+ * A Recipe data class contains recipe title,
+ * recipe instructions and ingredient list.
+ *
+ * @author Mindaugas Gailiunas
+ * @property title Recipe title
+ * @property instructions Recipe instructions
+ * @property ingredients a list of Recipe ingredients
+ */
 data class Recipe()
