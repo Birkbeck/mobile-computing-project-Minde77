57010e266a1235626d0c28f99e6ce5c3d08f193b
Mindaugas Gailiunas <mgaili01@student.bbk.ac.uk>
2025-05-31 16:52:59 +0100

    Add RecipeEditActivity class setup with documentation and comments

--- app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/RecipeEditActivity.kt
+++ app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/RecipeEditActivity.kt
@@ -4,6 +4,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import co.uk.bbk.coursework_mindaugasgailiunas.databinding.RecipeEditBinding
 
+/**
+ * A Recipe edit class. The class displays recipe edit data and input elements such as
+ * recipe title label and input, category label and dropdown menu, ingredients label
+ * and list elements with button to add ingredient, instruction label and text field
+ * and save and cancel recipe buttons.
+ *
+ * @author Mindaugas Gailiunas
+ */
 class RecipeEditActivity : AppCompatActivity() {
-    
+    // Bind all edit activity elements
+    private lateinit var binding: RecipeEditBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RecipeEditBinding.inflate(layoutInflater)
+
+        // Bind activity to view
+        val view = binding.root
+        setContentView(view) // Display bound elements on the screen
+    }
 }
