18326fe8c5392ac38d780fd820c24041cbaf97c1
Mindaugas Gailiunas <mgaili01@student.bbk.ac.uk>
2025-05-31 16:26:36 +0100

    Add RecipeViewActivity class setup with documentation and comments

--- app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/RecipeViewActivity.kt
+++ app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/RecipeViewActivity.kt
@@ -1,4 +1,26 @@
 package co.uk.bbk.coursework_mindaugasgailiunas
 
-class RecipeViewActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.uk.bbk.coursework_mindaugasgailiunas.databinding.RecipeViewBinding
+
+/**
+ * A Recipe view class. The class displays recipe data such as recipe title,
+ * category, ingredients, instructions and edit, back to dashboard and delete buttons.
+ *
+ * @author Mindaugas Gailiunas
+ */
+class RecipeViewActivity : AppCompatActivity() {
+
+    // Bind all recipe_view activity elements
+    private lateinit var binding: RecipeViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RecipeViewBinding.inflate(layoutInflater)
+
+        // Bind activity to view
+        val view = binding.root
+        setContentView(view) // Display bound elements on the screen
+    }
 }
