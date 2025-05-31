09550f197c1373f96d4401dc9e376fadae5a3c3f
Mindaugas Gailiunas <mgaili01@student.bbk.ac.uk>
2025-05-31 16:33:05 +0100

    Initial RecipeEditActivity class commit

---
+++ app/src/main/java/co/uk/bbk/coursework_mindaugasgailiunas/RecipeEditActivity.kt
@@ -0,0 +1,9 @@
+package co.uk.bbk.coursework_mindaugasgailiunas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.uk.bbk.coursework_mindaugasgailiunas.databinding.RecipeEditBinding
+
+class RecipeEditActivity : AppCompatActivity() {
+    
+}
